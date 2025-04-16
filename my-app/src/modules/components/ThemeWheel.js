a786d643c628beda1f36ff9905dea75dfe37e956
CloudTwelve <whippedreeeem@gmail.com>
2025-04-16 10:00:12 -0400

    start ThemeWheel css base, js base

---
+++ my-app/src/modules/components/ThemeWheel.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import './ThemeWheel.css';
+
+const ThemeWheel = () => {
+    <div class="wheel">
+        <div class="item-wrapper">
+            <div class="item"></div>
+        </div>
+        <div class="item-wrapper">
+            <div class="item"></div>
+        </div>
+        <div class="item-wrapper">
+            <div class="item"></div>
+        </div>
+    </div>
+};
+
+export default ThemeWheel;
+
